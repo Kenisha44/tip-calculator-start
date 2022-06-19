fa1005b568fa78d57789f0ce0771e8af88700661
Kenisha Johnson <kenishaajohnson@gmail.com>
2022-06-18 22:40:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,6 @@ round(bill_per_person, 2)
 final_amount = round(bill_per_person, 2)
 final_amount = "{:.2f}". format(bill_per_person) 
 print(f"Each person should pay ${final_amount}")
+
+
+#This is from Udemy course 
